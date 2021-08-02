ecf291d02f070c8f7a3ae62a097717ad56313435
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-08-02 01:26:39 +0000

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,12 @@
 <?php
 $a = $_POST['username'];
 $b = $_POST['domain'];
-$c = 'https://www.google.com/search?q=intext%3A%22'.$a.'@'. $b .'%22%20OR%20intext%3A%22'.$a.'%22';
+if (empty($a) or empty($a)) {
+    
+   echo '<script>window.location = "http://rasitech.herokuapp.com/"</script>';
+    
+
+} else {
+    $c = 'https://www.google.com/search?q=intext:"'.$a.'@'. $b .'" OR intext"'.$a.'"';
     echo '<script>window.location = "'. $c .'"</script>';
+}

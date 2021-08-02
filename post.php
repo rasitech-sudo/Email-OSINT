63701e6ed222fc9ce8449114d6091089ac84d222
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-08-02 01:01:14 +0000

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
 $a = $_POST['username'];
 $b = $_POST['domain'];
-$c = 'https://www.google.com/search?q=intext%3A%22'.$a.'@'. $b .'%22%20OR%20intext%22'.$a.'%22';
+$c = 'https://www.google.com/search?q=intext%3A%22'.$a.'@'. $b .'%22%20OR%20intext%3A%22'.$a.'%22';
     echo '<script>window.location = "'. $c .'"</script>';

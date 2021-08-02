f44d94521e19c57e8adc2bf7017a6ab854f991b1
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-08-02 00:44:49 +0000

    Update post.php

--- post.php
+++ post.php
@@ -3,7 +3,8 @@ $a = $_POST['username'];
 $b = $_POST['domain'];
 if (empty($a) or empty($b)) {
     
-   echo '<script>window.location = "http://rasitech.herokuapp.com/"</script>';
+   echo '<script>window.location = "http://rasitech.herokuapp.com/"; alert("Don't Empty");</script>';
+    
 
 } else {
     $c = 'https://www.google.com/search?q=intext:"'.$a.'@'. $b .'" OR intext"'.$a.'"';

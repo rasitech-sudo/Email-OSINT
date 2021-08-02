cbf93640c1236c0973d4b309d804478011e1a489
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-08-02 01:34:27 +0000

    Update post.php

--- post.php
+++ post.php
@@ -5,10 +5,6 @@ if (empty($a)) {
     
    echo '<script>window.location = "http://rasitech.herokuapp.com/"</script>';
     
-}elseif (empty($b)) {
-    
-   echo '<script>window.location = "http://rasitech.herokuapp.com/"</script>';
-    
 } else {
     $c = 'https://www.google.com/search?q=intext:"'.$a.'@'. $b .'" OR intext"'.$a.'"';
     echo '<script>window.location = "'. $c .'"</script>';
